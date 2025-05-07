60f7129334e1cd39ffbc4b475b6dc45402e68b87
zack pennachi <95557128+zpennachi@users.noreply.github.com>
2025-05-07 10:52:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Initialize Supabase
 const supabaseUrl      = 'https://sgvcogsjbwyfdvepalzf.supabase.co';
-const supabaseKey      = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseKey   = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNndmNvZ3NqYnd5ZmR2ZXBhbHpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzODQxNjgsImV4cCI6MjA1NDk2MDE2OH0.24hgp5RB6lwt8GRDGTy7MmbujkBv4FLstA-z5SOuqNo';
+
+
 const mySupabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 // UI Elements
